177363891017483587c0aa34ae3d059e4f85c1c5
Timothy Garcia <57849909+ainshield@users.noreply.github.com>
2025-03-31 03:46:47 +0800

    Update quiz_db.sql

--- quiz/quiz_db.sql
+++ quiz/quiz_db.sql
@@ -1,195 +1,220 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 30, 2025 at 09:37 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: quiz_db
+-- ------------------------------------------------------
+-- Server version	8.4.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `quiz_db`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admin_credentials`
 --
 
+DROP TABLE IF EXISTS `admin_credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin_credentials` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `admin_credentials`
 --
 
-INSERT INTO `admin_credentials` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin123'),
-(2, 'new', 'new123');
+LOCK TABLES `admin_credentials` WRITE;
+/*!40000 ALTER TABLE `admin_credentials` DISABLE KEYS */;
+INSERT INTO `admin_credentials` VALUES (1,'admin','admin123'),(2,'new','new123');
+/*!40000 ALTER TABLE `admin_credentials` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `modules`
+--
+
+DROP TABLE IF EXISTS `modules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modules` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `module_name` varchar(255) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modules`
+--
+
+LOCK TABLES `modules` WRITE;
+/*!40000 ALTER TABLE `modules` DISABLE KEYS */;
+INSERT INTO `modules` VALUES (1,'oralcomm','https://drive.google.com/file/d/1k613BIGa--Ss55Q1LHyu3yn6de3NwSCi/view');
+/*!40000 ALTER TABLE `modules` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `questions`
 --
 
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
-  `question` text NOT NULL,
-  `option1` varchar(255) NOT NULL,
-  `option2` varchar(255) NOT NULL,
-  `option3` varchar(255) NOT NULL,
-  `option4` varchar(255) NOT NULL,
-  `correct_option` varchar(255) NOT NULL,
-  `explanation` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question` text COLLATE utf8mb4_general_ci NOT NULL,
+  `option1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option2` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option3` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `option4` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `correct_option` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `explanation` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `option1`, `option2`, `option3`, `option4`, `correct_option`, `explanation`) VALUES
-(47, '5+5=?', '12', '10', '4', '5', 'B', 'haha\r\n');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-CREATE TABLE `students` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `lrn` varchar(255) NOT NULL,
-  `strand` varchar(50) NOT NULL,
-  `grade_level` varchar(10) NOT NULL,
-  `section` varchar(100) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`id`, `first_name`, `last_name`, `lrn`, `strand`, `grade_level`, `section`, `username`, `password`) VALUES
-(1, 'raf', 'gayatao', '09983186065', 'STEM', '12', 'rizal', 'raf', 'gayatao');
-
--- --------------------------------------------------------
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (47,'5+5=?','12','10','4','5','B','haha\r\n'),(56,'updated answer','a','b','c','d','C','updated answer'),(57,'asdasdasd','a','b','c','d','D',''),(58,'gewgewg','a','b','c','d','D',''),(59,'ahfdhdfah','a','b','c','d','D','');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `student_credentials`
 --
 
+DROP TABLE IF EXISTS `student_credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student_credentials` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `score` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `score` int DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `student_credentials`
+--
+
+LOCK TABLES `student_credentials` WRITE;
+/*!40000 ALTER TABLE `student_credentials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student_credentials` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `student_registration`
 --
 
+DROP TABLE IF EXISTS `student_registration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student_registration` (
-  `first_name` int(11) NOT NULL,
-  `last_name` int(11) NOT NULL,
-  `lrn` int(11) NOT NULL,
-  `strand` int(11) NOT NULL,
-  `grade_level` int(11) NOT NULL,
-  `section` int(11) NOT NULL,
-  `username` int(11) NOT NULL,
-  `password` int(11) NOT NULL
+  `first_name` int NOT NULL,
+  `last_name` int NOT NULL,
+  `lrn` int NOT NULL,
+  `strand` int NOT NULL,
+  `grade_level` int NOT NULL,
+  `section` int NOT NULL,
+  `username` int NOT NULL,
+  `password` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `student_registration`
 --
 
-INSERT INTO `student_registration` (`first_name`, `last_name`, `lrn`, `strand`, `grade_level`, `section`, `username`, `password`) VALUES
-(0, 0, 0, 0, 0, 0, 0, 0),
-(0, 0, 0, 0, 0, 0, 0, 0),
-(0, 0, 0, 0, 0, 0, 0, 0),
-(0, 0, 0, 0, 0, 0, 0, 0),
-(0, 0, 0, 0, 0, 0, 0, 0);
+LOCK TABLES `student_registration` WRITE;
+/*!40000 ALTER TABLE `student_registration` DISABLE KEYS */;
+INSERT INTO `student_registration` VALUES (0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `student_registration` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for dumped tables
+-- Table structure for table `students`
 --
 
---
--- Indexes for table `admin_credentials`
---
-ALTER TABLE `admin_credentials`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `lrn` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `strand` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `grade_level` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `section` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for table `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `student_credentials`
---
-ALTER TABLE `student_credentials`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `students`
 --
 
---
--- AUTO_INCREMENT for table `admin_credentials`
---
-ALTER TABLE `admin_credentials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'raf','gayatao','09983186065','STEM','12','rizal','raf','gayatao');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- AUTO_INCREMENT for table `questions`
+-- Table structure for table `users`
 --
-ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `role` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT for table `students`
+-- Dumping data for table `users`
 --
-ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `student_credentials`
---
-ALTER TABLE `student_credentials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-COMMIT;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'user','user','user'),(2,'123','123','admin'),(3,'kups','123','user');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-31  3:46:02
